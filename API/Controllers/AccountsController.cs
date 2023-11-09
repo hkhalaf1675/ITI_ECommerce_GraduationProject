9418038569cc49be3e339e9bbe8bf497a02f7805
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-09 14:16:10 +0200

    fix Conflict

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -19,11 +19,15 @@ namespace API.Controllers
     {
         private readonly UserManager<User> userManager;
         private readonly IConfiguration configuration;
+        private readonly IWishlistRepository wishlistRepository;
+        private readonly IFavouriteRepository favouriteRepository;
 
-        public AccountsController(UserManager<User> _userManager,IConfiguration _configuration)
+        public AccountsController(UserManager<User> _userManager, IConfiguration _configuration, IWishlistRepository _wishlistRepository, IFavouriteRepository _favouriteRepository)
         {
             this.userManager = _userManager;
             this.configuration = _configuration;
+            wishlistRepository = _wishlistRepository;
+            favouriteRepository = _favouriteRepository;
         }
 
         [HttpPost]
