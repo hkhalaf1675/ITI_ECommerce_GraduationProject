5ad07bd0d19824b029428c486fa829c8ae13e1da
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-13 11:14:58 +0200

    update

--- Infrastructure/Repositories/ShopingCartRepository.cs
+++ Infrastructure/Repositories/ShopingCartRepository.cs
@@ -62,25 +62,31 @@ namespace Infrastructure.Repositories
 
         public async Task<ICollection<CartProductsDto>> GetUserCartProducts(int userId)
         {
+
+            // modified was returning single value 
+            List<ShopingCart> shoppingcart = new List<ShopingCart>();
             List<CartProductsDto> cartProducts = new List<CartProductsDto>();
 
-            var userCarts = context.ShoppingCarts.Where(C => C.UserID == userId);
-            if(userCarts?.Count() == 0)
+            shoppingcart = context.ShoppingCarts.Where(C => C.UserID == userId).ToList();
+
+            if (shoppingcart == null)
             {
                 return cartProducts;
             }
 
-            foreach(var userCart in userCarts)
+            foreach (var item in shoppingcart)
             {
-                Product product = context.Products.FirstOrDefault(P => P.Id == userCart.ProductID);
+
+                Product product = context.Products.FirstOrDefault(P => P.Id == item.ProductID);
                 cartProducts.Add(new CartProductsDto
                 {
-                    ProductId = userCart.ProductID,
-                    ProductName = product?.Name,
-                    ProductPrice = product?.Price,
-                    ProductQuantity = userCart.Quantity,
-                    Discount = (product?.Discount == null) ? 0 : product.Discount
+                    ProductId = item.ProductID,
+                    ProductName = item.Product.Name,
+                    ProductPrice = item.Product.Price,
+                    ProductQuantity = item.Quantity,
+                    Discount = (item.Product.Discount == null) ? 0 : item.Product.Discount
                 });
+
             }
 
             return cartProducts;
