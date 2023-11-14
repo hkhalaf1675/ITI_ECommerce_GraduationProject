e57447ad801bb17b8c6e68249433bb9f1824e20d
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-14 23:51:30 +0200

    Update

---
+++ Infrastructure/Migrations/20231114125559_Mi1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mi1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "OrdersDetails",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "OrdersDetails",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostalCode",
+                table: "Address",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressId",
+                table: "Orders",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Address_AddressId",
+                table: "Orders",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Address_AddressId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AddressId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "OrdersDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "OrdersDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostalCode",
+                table: "Address",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
