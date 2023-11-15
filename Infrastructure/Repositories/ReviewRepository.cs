ef42e8ccbc86d532e4abe19cf0988bb7d048b9fd
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-11-15 14:49:11 +0200

    # Fix Get All Reviews - make it handle out of bound page index and null page index

--- Infrastructure/Repositories/ReviewRepository.cs
+++ Infrastructure/Repositories/ReviewRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Core.IRepositories;
 using Core.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -30,11 +31,35 @@ namespace Infrastructure.Repositories
         public ICollection<Review> GetAll(int? pageIndex)
         {
             var reviews = _context.Reviews
-                        .Include(r => r.Product)
-                        .Include(r => r.User)
-            .ToList();
-            return reviews.Skip((int)((pageIndex - 1) * 6)).Take(6).ToList(); 
+                .Include(r => r.Product)
+                .Include(r => r.User)
+                .ToList();
+
+
+            // At first we have to check that the page index has a value in the first place before proceed on 
+            if (pageIndex.HasValue && pageIndex > 0)
+            {
+                int startIndex = (pageIndex.Value - 1) * 6;
+
+                // Here we make sure to not going out of bound!
+                if (startIndex >= 0 && startIndex < reviews.Count)
+                {
+                    return reviews.Skip(startIndex).Take(6).ToList();
+                }
+                else
+                {
+                    // In case it went out of bound we send back an empty list     
+                    return new List<Review>();
+                }
+            }
+            else
+            {
+                // In case the page index is null here, we send back an empty list 
+                return new List<Review>();
+            }
         }
+
+
         public ICollection<Review> GetReviewsByProduct(int productId)
         {
             return _context.Reviews
