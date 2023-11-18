ca71586a0609f2687169255ac32e675dcde8fe47
Hassan Tawfik <h.khalaf16.75@gmail.com>
2023-11-18 23:21:00 +0200

    add admin report

---
+++ API/Controllers/AdminReportController.cs
@@ -0,0 +1,33 @@
+﻿using Core.IRepositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize(Roles = "Admin")]
+    public class AdminReportController : ControllerBase
+    {
+        private readonly IAdminReportRepository reportRepository;
+
+        public AdminReportController(IAdminReportRepository _reportRepository)
+        {
+            reportRepository = _reportRepository;
+        }
+
+        [HttpGet("GetByDate")]
+        public async Task<IActionResult> GetByDate(DateTime startDate,DateTime endDate)
+        {
+            var report = await reportRepository.GetReportByDate(startDate, endDate);
+
+            if(report == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(report);
+        }
+    }
+}
--- Core/DTOs/UserProfileDtos/UserUpdateDto.cs
+++ Core/DTOs/UserProfileDtos/UserUpdateDto.cs
@@ -8,7 +8,10 @@ namespace Core.DTOs.UserProfileDtos
 {
     public class UserUpdateDto
     {
-        public string FullName { get; set; }
-        public string Email { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
     }
 }
